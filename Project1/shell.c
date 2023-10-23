2a4c25bc27bedea270e9bf3e8793e95f0dc85b6e
Brandon Clarke <brandon.clarke1430@gmail.com>
2023-10-23 11:18:22 -0400

    add set up for shell file

---
+++ Project1/shell.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define MAX_COMMAND_LINE_LEN 1024
+#define MAX_COMMAND_LINE_ARGS 128
+
+char prompt[] = "> ";
+char delimiters[] = " =\t\r\n";
+extern char **environ;
+bool background = false;
+int chld_term = 0;
+int timer_set = 0;
