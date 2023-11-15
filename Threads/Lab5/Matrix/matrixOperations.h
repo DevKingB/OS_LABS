2e0fd134953b4509985bfc507a3a13e8b1192b41
Brandon Clarke <brandon.clarke1430@gmail.com>
2023-11-15 14:46:06 -0500

    Remove static variable, implement matrix.c file, update include files

--- Threads/Lab5/Matrix/matrixOperations.h
+++ Threads/Lab5/Matrix/matrixOperations.h
@@ -1,26 +1,32 @@
 #ifndef MATRIX_OPERATIONS_H
 #define MATRIX_OPERATIONS_H
 
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
 
-//Defining a object to be used with threads
+// Defining variables for matrix operations
+extern int *matA, *matB;
+extern int *matSumResult, *matDiffResult, *matProductResult;
+
+// Defining a structure to be used with threads
 typedef struct {
     int startRow;
     int endRow;
+    int MAX; // Including MAX in the ThreadArg structure
 } ThreadArg;
 
-/* Declaring Matrix Implementation functions */
+/* Declaring Matrix Operation functions */
 int* allocateMatrix(int MAX);
-void initializeMatrices(int size);
+void initializeMatrices(int MAX);
 void freeGlobalMatrices();
-void fillMatrix(int *matrix, int size);
-void printMatrix(int *matrix, int size);
-void performOperation(void *(*operation)(void *), int size);
-void *computeSum(void *arg, int MAX);
-void *computeProduct(void *arg, int MAX);
-void *computeDiff(void *arg, int MAX);
+void fillMatrix(int *matrix, int MAX);
+void printMatrix(int *matrix, int MAX);
+void performOperation(void *(*operation)(void *), int MAX);
+void *computeSum(void *arg);
+void *computeDiff(void *arg);
+void *computeProduct(void *arg);
 
-#endif //MATRIX_OPERATION_H
+#endif // MATRIX_OPERATIONS_H
